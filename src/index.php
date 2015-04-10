270684eb4ac90a818cb3fe43a8e2aebbf5a3a304
RomainQuinaud <quinaud.romain@gmail.com>
2015-04-10 09:54:15 +0200

    php storm project first commit

---
+++ src/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+
+    <meta name="author" content="">
+
+    <link rel="icon" href="../img/photo-justice.ico">
+
+    <title>Hellaw's</title>
+
+    <!-- Bootstrap core CSS -->
+
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+
+    <link href="../css/starter-template.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+
+    <!--[if lt IE 9]>
+
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+
+  </head>
+
+  <body>
+
+    <?php
+    include 'menu.php';
+    ?>
+
+    
+    <div class="container">
+
+      <div class="starter-template">
+
+        <h1>Bienvenue sur Hellaw's</h1>
+
+        <p class="lead">Ce site contient toutes les informations nécessaires en cas de recours à la justice<br> All you get is this text and a mostly barebones HTML document.</p>
+
+	<div class="row">
+        <div class="col-sm-9 blog-main">
+        	<div class="blog-post">
+        		<h2 class="blog-post-titre">Loi du Jour</h2>
+           			<p>La loi du jour n'est pas disponible... Ce n'est pas le bon jour.</p>
+    		</div>
+    		<div class="blog-post">
+    			<h2>Article</h2>
+	            	<p>Emplacement du futur emplacement du texte de l'article.<br>BLA... BLA...<br>BLA... BLA...</p>
+	        </div>
+    	</div>
+        <div class="col-sm-2 col-sm-offset-1 blog-sidebar">
+	        <div class="sidebar-module sidebar-module-inset">
+	           	<div class="sidebar-module">
+                	<h2>Notification</h2>
+               			<p>Emplacement de la future notification.</p>
+            	</div>
+        	</div>
+    	</div>
+
+
+    <footer class="row">
+       	<div class="col-lg-12">
+    	  	Pied de page
+        </div>
+    </footer> 
+
+
+  </div></div>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+
+    ================================================== -->
+
+    <!-- Placed at the end of the document so the pages load faster -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <script src="../../../../../../Users/Romain%20QUINAUD/Desktop/save/Projet/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+
+  </body>
+
+</html>
