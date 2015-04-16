e2b3197341b812e035ddbca13ec30e566bd98756
RomainQuinaud <quinaud.romain@gmail.com>
2015-04-16 14:12:45 +0200

    Clean up code by romving commentary

--- src/reservation.php
+++ src/reservation.php
@@ -45,52 +45,7 @@
 </head>
 
 <body>
-<!--
-<div class="navbar navbar-inverse navbar-fixed-top">
 
-    <div class="container">
-
-        <div class="navbar-header">
-
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-
-                <span class="sr-only">Toggle navigation</span>
-
-                <span class="icon-bar"></span>
-
-                <span class="icon-bar"></span>
-
-                <span class="icon-bar"></span>
-
-            </button>
-
-            <a class="navbar-brand" href="infos.php">Camping Paradis</a>
-
-        </div>
-
-
-        <div id="navbar" class="collapse navbar-collapse">
-
-            <ul class="nav navbar-nav">
-
-                <li><a href="index.php">Accueil</a></li>
-
-                <li class="active"><a href="reservation.php">Réservation</a></li>
-
-            </ul>
-
-            <ul class="nav navbar-nav navbar-right">
-
-                <li><a href="deco.php">Déconnexion</a></li>
-
-            </ul>
-
-        </div><!--/.nav-collapse -->
-<!--
-    </div>
-
-</div>
--->
 <?php
 include_once 'menu.php';
 menu("reservation.php");
@@ -100,74 +55,8 @@ menu("reservation.php");
 
     <div class="starter-template">
       <h1>Recherche</h1>
-      <p>
-        <label>Que désirez-vous recherchez?</label>
-      </br>
-      <input type="radio" name="type_recherche" value="loi" id="loi" /> <!--checked="checked"--> <label for="oui">Une Loi</label></br>
-      <input type="radio" name="type_recherche" value="cabinet" id="cabinet" /> <label for="non">Un Cabinet</label>
-    </p>
-    </div>
-    <div class="starter-template">
-      <label>Recherche par Thématique : </label>
-      <select name="thématique">
-        <option value="enfance" >Droit de l'enfance</option>
-        <option value="travail" >Droit du travail</option>
-        <option value="prostitution" >Droit lié à la prostitution</option>
-      </select>
-      <label>Recherche par Date de Publication</label>
-      <select name="mois">
-        <option value="janvier" >Janvier</option>
-        <option value="fevrier" >Février</option>
-        <option value="mars" >Mars</option>
-        <option value="avril">Avril</option>
-        <option value="mai">Mai</option>
-        <option value="juin">Juin</option>
-        <option value="juillet">Juillet</option>
-        <option value="août">Août</option>
-        <option value="septembre">Septembre</option>
-        <option value="octobre">Octobre</option>
-        <option value="novembre">Novembre</option>
-        <option value="décembre">Décembre</option>
-      </select>
-      <select name="annee">
-        <?php 
-        for($i=1980;$i<2016;$i++) {
-          echo '<option value="'.$i.'">'.$i.'</option>';
-        }
-        ?>
-      </select>
-      <div class="col-lg-12">
-        <form>
-          <div class="form-group">
-            <label for="Departement">Rechercher par département</label>
-            <select name="Departement" >
-              <?php 
-              for($i=1;$i<=19;$i++)
-              { 
-                echo '<OPTION value="'.$i.'"">'.$i.'</OPTION>';
-              }
-              ?>
 
-              <option> 2A </option>
-              <option>2B</option>
 
-              <?php 
-              for($i=21;$i<=95;$i++)
-              { 
-                echo '<OPTION value="'.$i.'"">'.$i.'</OPTION>';
-              }
-              ?>
-
-              <?php 
-              for($i=971;$i<=976;$i++)
-              { 
-                echo '<OPTION value="'.$i.'"">'.$i.'</OPTION>';
-              }
-              ?>
-            </select>
-          </div>
-        </form>
-      </div>
     </div>
   </div>
 </body>
