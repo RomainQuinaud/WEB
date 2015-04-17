2899dd9bfaff15c049c1ad125b51b1f0a67a2c23
RomainQuinaud <quinaud.romain@gmail.com>
2015-04-17 08:28:14 +0200

    minor changes

--- src/connexion.php
+++ src/connexion.php
@@ -7,34 +7,20 @@ if(isset($_SESSION['login']))
 
 
 <!DOCTYPE html>
-<html lang="fr" xmlns="http://www.w3.org/1999/html">
-  <head>
+<html lang="fr">
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../img/camping.ico">
-
-    <title>Hellaw's</title>
-
+    <title>Camping Paradis</title>
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="../css/signin-template.css" rel="stylesheet">
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-
-    <!--[warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media
-    <!-- queries -->
-    <!--[if lt IE 9]>
-      <script
-      <src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script
-      <src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+</head>
 
   <body>
 
--- src/reservation.php
+++ src/reservation.php
@@ -1,32 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['login']))
+    header('Location: connexion.php');
+?>
 <!DOCTYPE html>
-
 <html lang="fr">
-
 <head>
-
   <meta charset="utf-8">
-
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <meta name="description" content="">
-
   <meta name="author" content="">
-
   <link rel="icon" href="../img/camping.ico">
-
   <title>Camping Paradis</title>
-
   <!-- Bootstrap core CSS -->
-
   <link href="../css/bootstrap.min.css" rel="stylesheet">
-
   <!-- Custom styles for this template -->
-
   <link href="../css/starter-template.css" rel="stylesheet">
-
-
 </head>
 
 <body>
