344529db0afa5a2ff04987e9e2c64785e6d4c92c
RomainQuinaud <quinaud.romain@gmail.com>
2015-05-01 15:14:15 +0200

    Rajout barre deroulante

--- src/infos.php
+++ src/infos.php
@@ -53,7 +53,7 @@ if(!isset($_SESSION['login']))
 <!-- Placed at the end of the document so the pages load faster -->
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
+<script src="../js/bootstrap.min.js"></script>
 
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 
