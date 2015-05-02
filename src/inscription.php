cbe0a82f2cb0ab08f03dc9cb922dce2a104f3edc
RomainQuinaud <quinaud.romain@gmail.com>
2015-05-02 13:30:21 +0200

    Datepicker

--- src/inscription.php
+++ src/inscription.php
@@ -19,11 +19,13 @@ if(isset($_SESSION['login']))
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="../css/signin-template.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/datepicker.css">
 
 
 </head>
 
 <body>
+
 
 <div class="container">
 
@@ -112,6 +114,12 @@ if(isset($_SESSION['login']))
                 } ?>
             </div>
 
+            <div class="input-daterange input-group" id="datepicker">
+                <input type="text" class="input-sm form-control" name="start"/>
+                <span class="input-group-addon">to</span>
+                <input type="text" class="input-sm form-control" name="end"/>
+            </div>
+
 
             <div class="form-group">
                 <button type="submit" class="btn btn-default">Inscription</button>
@@ -147,6 +155,27 @@ if(isset($_SESSION['login']))
 <script src="../../../../../../Users/Romain%20QUINAUD/Desktop/save/Projet/bootstrap/js/bootstrap.min.js"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+<script src="../js/jquery-1.9.1.min.js"></script>
+<script src="../js/bootstrap-datepicker.js"></script>
+<script type="text/javascript">
+    // When the document is ready
+    $(document).ready(function () {
+
+        $('.input-daterange').datepicker({
+            format: "yyyy-mm-dd",
+            todayBtn: "linked",
+            clearBtn: true,
+            language: "fr",
+            forceParse: false,
+            daysOfWeekDisabled: "0,1,2,3,4,5",
+            autoclose: true,
+            todayHighlight: true,
+
+            datesDisabled: ['05/06/2015', '05/21/2015']
+        });
+
+    });
+</script>
 
 
 </body>
