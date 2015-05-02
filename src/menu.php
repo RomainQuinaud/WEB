a80cf87c34b28f3ff84c3ba2e33cf94b1dc8fb4a
RomainQuinaud <quinaud.romain@gmail.com>
2015-05-02 11:30:57 +0200

    test

--- src/menu.php
+++ src/menu.php
@@ -6,143 +6,98 @@
 function menu($page)
 {
     ?>
-
     <!--
 
- Fixed navbar
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header"></div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#about">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#contact">
+                                Contact
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown" href="#">
+                                Dropdown
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="#">
+                                        Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Another action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Something else here
+                                    </a>
+                                </li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">
+                                    Nav header
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        Separated link
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        One more separated link
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
 
--->
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="../navbar/">
+                                Default
+                            </a>
+                        </li>
+                        <li>
+                            <a href="../navbar-static-top/">
+                                Static top
 
-    <nav class="navbar navbar-default navbar-fixed-top">
+                            </a>
+                        </li>
+                        <li class="active">
+                            <a href="./">
+                                Fixed top
+                            <span class="sr-only">
+                                (current)
+                            </span>
+                            </a>
+                        </li>
 
+                    </ul>
 
-        <div class="container">
+                </div>
 
-            <div class="navbar-header"></div>
-            <div id="navbar" class="navbar-collapse collapse">
-
-                <ul class="nav navbar-nav">
-
-                    <li class="active">
-                        <a href="#">
-
-                            Home
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#about">
-
-                            About
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#contact">
-
-                            Contact
-
-                        </a>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown" href="#">
-
-                            Dropdown
-
-                            <span class="caret"></span>
-
-                        </a>
-                        <ul class="dropdown-menu" role="menu">
-
-                            <li>
-                                <a href="#">
-
-                                    Action
-
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-
-                                    Another action
-
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-
-                                    Something else here
-
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">
-
-                                Nav header
-
-                            </li>
-                            <li>
-                                <a href="#">
-
-                                    Separated link
-
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#">
-
-                                    One more separated link
-
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-
-                    <li>
-                        <a href="../navbar/">
-
-                            Default
-
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../navbar-static-top/">
-
-                            Static top
-
-                        </a>
-                    </li>
-                    <li class="active">
-                        <a href="./">
-
-                            Fixed top
-
-                        <span class="sr-only">
-
-                            (current)
-
-                        </span>
-                        </a>
-                    </li>
-
-                </ul>
 
             </div>
-            <!--
-
-            /.nav-collapse
-
-            -->
-
-        </div>
 
 
-    </nav>
+        </nav>-->
 
-    <!--
 
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -194,7 +149,7 @@ function menu($page)
         </div>
 
     </nav>
--->
+
 
 <?php
 }
