17f3f469c9dcf43a951a4c712b399cd19e16369b
RomainQuinaud <quinaud.romain@gmail.com>
2015-05-02 11:27:59 +0200

    test

--- src/menu.php
+++ src/menu.php
@@ -7,8 +7,142 @@ function menu($page)
 {
     ?>
 
+    <!--
+
+ Fixed navbar
+
+-->
+
+    <nav class="navbar navbar-default navbar-fixed-top">
 
 
+        <div class="container">
+
+            <div class="navbar-header"></div>
+            <div id="navbar" class="navbar-collapse collapse">
+
+                <ul class="nav navbar-nav">
+
+                    <li class="active">
+                        <a href="#">
+
+                            Home
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#about">
+
+                            About
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact">
+
+                            Contact
+
+                        </a>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" aria-expanded="false" role="button" data-toggle="dropdown" href="#">
+
+                            Dropdown
+
+                            <span class="caret"></span>
+
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+
+                            <li>
+                                <a href="#">
+
+                                    Action
+
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+
+                                    Another action
+
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+
+                                    Something else here
+
+                                </a>
+                            </li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">
+
+                                Nav header
+
+                            </li>
+                            <li>
+                                <a href="#">
+
+                                    Separated link
+
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+
+                                    One more separated link
+
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li>
+                        <a href="../navbar/">
+
+                            Default
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="../navbar-static-top/">
+
+                            Static top
+
+                        </a>
+                    </li>
+                    <li class="active">
+                        <a href="./">
+
+                            Fixed top
+
+                        <span class="sr-only">
+
+                            (current)
+
+                        </span>
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+            <!--
+
+            /.nav-collapse
+
+            -->
+
+        </div>
+
+
+    </nav>
+
+    <!--
 
 
     <nav class="navbar navbar-inverse navbar-fixed-top">
@@ -45,10 +179,11 @@ function menu($page)
                     <li <?php if ($page == 'reservation.php') echo 'class="active"' ?>><a href="reservation.php">Réservation</a>
                     </li>
 
-
+                    <ul class="nav navbar-nav navbar-right">
                     <li><a> Connecté en tant que: <?php echo $_SESSION['login']?> </a></li>
 
                     <li><a href="deco.php">Déconnexion</a></li>
+                    </ul>
 
 
                 </ul>
@@ -59,7 +194,7 @@ function menu($page)
         </div>
 
     </nav>
-
+-->
 
 <?php
 }
