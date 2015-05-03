c032953a84c982a6a11691d82607a41e61b7a00c
philippealeixo <aleixo.philippe@gmail.com>
2015-05-03 14:36:06 +0200

    Remplissage site

--- src/insert_reservation.php
+++ src/insert_reservation.php
@@ -4,12 +4,16 @@ session_start();
 include 'BDD_connect.php';
 
 
-//Cette requetedonne tous les logements deja reservés pour cette période
-$verifdate = $pdo->prepare("SELECT idlogement FROM logement NATURAL JOIN reservation
+//Cette requete donne tous les logements deja reservés pour la période sélectionnée
+$verifdateStatements = $pdo->prepare("SELECT idlogement FROM logement NATURAL JOIN reservation
                           WHERE (datedebut<:mondebut AND datefin>:mafin)
                           OR (datefin>:mondebut AND datefin<:mafin)
                           OR (datedebut<:mondebut AND datedebut>:mafin)");
-$verifdate->bindParam(':mondebut', $_POST['start']);
-$verifdate->bindParam(':mafin', $_POST['end']);
-$verifdate->execute();
+$verifdateStatements->bindParam(':mondebut', $_POST['start']);
+$verifdateStatements->bindParam(':mafin', $_POST['end']);
+$verifdateStatements->execute();
+
+//Cette requete donne une liste de tous les logements
+$logementStatements = $pdo->prepare("SELECT idlogement FROM logement");
+$logementStatements->execute();
 
