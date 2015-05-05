608ac37926141d700e1fe0fc4533e42bc256eb15
RomainQuinaud <quinaud.romain@gmail.com>
2015-05-05 05:26:02 +0200

    Maj

--- camping.sql
+++ camping.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.12
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Lun 04 Mai 2015 à 18:25
+-- Client :  127.0.0.1
+-- Généré le :  Lun 04 Mai 2015 à 19:32
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -17,47 +17,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données :  `camping`
+-- Base de données :  camping
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `camping`
+-- Structure de la table camping
 --
 
-CREATE TABLE IF NOT EXISTS `camping` (
-  `idcamping` int(11) NOT NULL,
-  `nomcamping` varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `villecamping` varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `adressecamping` varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `departementcamping` int(2) unsigned zerofill DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS camping (
+  idcamping int(11) NOT NULL AUTO_INCREMENT,
+  nomcamping varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  villecamping varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  adressecamping varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  departementcamping int(2) unsigned zerofill DEFAULT NULL,
+  PRIMARY KEY (idcamping)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci AUTO_INCREMENT=2 ;
 
 --
--- Contenu de la table `camping`
+-- Contenu de la table camping
 --
 
-INSERT INTO `camping` (`idcamping`, `nomcamping`, `villecamping`, `adressecamping`, `departementcamping`) VALUES
+INSERT INTO camping (idcamping, nomcamping, villecamping, adressecamping, departementcamping) VALUES
 (1, 'Les Flots Bleus', 'taratata les bains', '10 rue de la plage', 10);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
+-- Structure de la table categorie
 --
 
-CREATE TABLE IF NOT EXISTS `categorie` (
-  `idcategorie` int(11) NOT NULL,
-  `libellecategorie` varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `prixcategorie` int(5) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS categorie (
+  idcategorie int(11) NOT NULL AUTO_INCREMENT,
+  libellecategorie varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  prixcategorie int(5) DEFAULT NULL,
+  PRIMARY KEY (idcategorie)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci AUTO_INCREMENT=4 ;
 
 --
--- Contenu de la table `categorie`
+-- Contenu de la table categorie
 --
 
-INSERT INTO `categorie` (`idcategorie`, `libellecategorie`, `prixcategorie`) VALUES
+INSERT INTO categorie (idcategorie, libellecategorie, prixcategorie) VALUES
 (1, 'Bungallow', 75),
 (2, 'Tente', 40),
 (3, 'Caravane', 120);
@@ -65,22 +67,26 @@ INSERT INTO `categorie` (`idcategorie`, `libellecategorie`, `prixcategorie`) VAL
 -- --------------------------------------------------------
 
 --
--- Structure de la table `logement`
+-- Structure de la table logement
 --
 
-CREATE TABLE IF NOT EXISTS `logement` (
-  `idlogement` int(11) NOT NULL,
-  `idcategorie` int(5) DEFAULT NULL,
-  `nomlogement` varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `idcamping` int(11) DEFAULT NULL,
-  `image` varchar(100) COLLATE utf8_unicode_520_ci DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS logement (
+  idlogement int(11) NOT NULL AUTO_INCREMENT,
+  idcategorie int(5) DEFAULT NULL,
+  nomlogement varchar(50) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  idcamping int(11) DEFAULT NULL,
+  image varchar(100) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  PRIMARY KEY (idlogement),
+  UNIQUE KEY nomlogement (nomlogement),
+  KEY FK_logement_camping (idcamping),
+  KEY FK_logement_categorie (idcategorie)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci AUTO_INCREMENT=30 ;
 
 --
--- Contenu de la table `logement`
+-- Contenu de la table logement
 --
 
-INSERT INTO `logement` (`idlogement`, `idcategorie`, `nomlogement`, `idcamping`, `image`) VALUES
+INSERT INTO logement (idlogement, idcategorie, nomlogement, idcamping, image) VALUES
 (1, 1, 'Requin', 1, '../img/logement/logement_requin.jpg'),
 (2, 1, 'Dauphin', 1, '../img/logement/logement_dauphin.jpg'),
 (3, 1, 'Baleine', 1, '../img/logement/logement_baleine.jpg'),
@@ -114,52 +120,28 @@ INSERT INTO `logement` (`idlogement`, `idcategorie`, `nomlogement`, `idcamping`,
 -- --------------------------------------------------------
 
 --
--- Structure de la table `pourcent_reduc`
+-- Structure de la table prix_periode
 --
 
-CREATE TABLE IF NOT EXISTS `pourcent_reduc` (
-  `avance` varchar(5) COLLATE utf8_unicode_520_ci NOT NULL DEFAULT '',
-  `pourcent` int(11) NOT NULL
+CREATE TABLE IF NOT EXISTS prix_periode (
+  mois varchar(30) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  ajout int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
 
 --
--- Contenu de la table `pourcent_reduc`
+-- Contenu de la table prix_periode
 --
 
-INSERT INTO `pourcent_reduc` (`avance`, `pourcent`) VALUES
-('0-1', 4),
-('10-11', 9),
-('12', 10),
-('2-3', 5),
-('4-5', 6),
-('6-7', 7),
-('8-9', 8);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `prix_periode`
---
-
-CREATE TABLE IF NOT EXISTS `prix_periode` (
-  `mois` varchar(30) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `ajout` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
-
---
--- Contenu de la table `prix_periode`
---
-
-INSERT INTO `prix_periode` (`mois`, `ajout`) VALUES
+INSERT INTO prix_periode (mois, ajout) VALUES
 ('January', 10),
 ('February', 25),
-('February', 25),
+('March', 25),
 ('April', 25),
 ('May', 35),
 ('June', 35),
 ('July', 50),
 ('August', 50),
-('August', 35),
+('September', 35),
 ('October', 25),
 ('November', 25),
 ('December', 15);
@@ -167,23 +149,26 @@ INSERT INTO `prix_periode` (`mois`, `ajout`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reservation`
+-- Structure de la table reservation
 --
 
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `numreservation` int(11) NOT NULL,
-  `idUTILISATEUR` int(11) DEFAULT NULL,
-  `idlogement` int(11) DEFAULT NULL,
-  `datereservation` datetime DEFAULT NULL,
-  `datedebut` date DEFAULT NULL,
-  `datefin` date DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS reservation (
+  numreservation int(11) NOT NULL AUTO_INCREMENT,
+  idUTILISATEUR int(11) DEFAULT NULL,
+  idlogement int(11) DEFAULT NULL,
+  datereservation datetime DEFAULT NULL,
+  datedebut date DEFAULT NULL,
+  datefin date DEFAULT NULL,
+  PRIMARY KEY (numreservation),
+  KEY FK_reservation_utilisateur (idUTILISATEUR),
+  KEY FK_reservation_logement (idlogement)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci AUTO_INCREMENT=16 ;
 
 --
--- Contenu de la table `reservation`
+-- Contenu de la table reservation
 --
 
-INSERT INTO `reservation` (`numreservation`, `idUTILISATEUR`, `idlogement`, `datereservation`, `datedebut`, `datefin`) VALUES
+INSERT INTO reservation (numreservation, idUTILISATEUR, idlogement, datereservation, datedebut, datefin) VALUES
 (1, 1, 1, '2015-04-25 11:15:05', '2015-07-01', '2015-07-08'),
 (9, 1, 1, '2015-05-03 17:40:45', '2015-07-08', '2015-07-15'),
 (12, 1, 1, '2015-05-03 22:17:23', '2015-04-25', '2015-05-02'),
@@ -194,116 +179,50 @@ INSERT INTO `reservation` (`numreservation`, `idUTILISATEUR`, `idlogement`, `dat
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Structure de la table utilisateur
 --
 
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `idUTILISATEUR` int(11) NOT NULL,
-  `loginUTILISATEUR` varchar(40) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `nomUTILISATEUR` varchar(60) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `prenomUTILISATEUR` varchar(60) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `telephoneUTILISATEUR` int(10) unsigned zerofill DEFAULT NULL,
-  `mailUTILISATEUR` varchar(40) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `departementUTILISATEUR` int(3) unsigned zerofill DEFAULT NULL,
-  `mdpUTILISATEUR` varchar(100) COLLATE utf8_unicode_520_ci DEFAULT NULL,
-  `admin` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
+CREATE TABLE IF NOT EXISTS utilisateur (
+  idUTILISATEUR int(11) NOT NULL AUTO_INCREMENT,
+  loginUTILISATEUR varchar(40) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  nomUTILISATEUR varchar(60) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  prenomUTILISATEUR varchar(60) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  telephoneUTILISATEUR int(10) unsigned zerofill DEFAULT NULL,
+  mailUTILISATEUR varchar(40) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  departementUTILISATEUR int(3) unsigned zerofill DEFAULT NULL,
+  mdpUTILISATEUR varchar(100) COLLATE utf8_unicode_520_ci DEFAULT NULL,
+  admin tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (idUTILISATEUR)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci AUTO_INCREMENT=3 ;
 
 --
--- Contenu de la table `utilisateur`
+-- Contenu de la table utilisateur
 --
 
-INSERT INTO `utilisateur` (`idUTILISATEUR`, `loginUTILISATEUR`, `nomUTILISATEUR`, `prenomUTILISATEUR`, `telephoneUTILISATEUR`, `mailUTILISATEUR`, `departementUTILISATEUR`, `mdpUTILISATEUR`, `admin`) VALUES
+INSERT INTO utilisateur (idUTILISATEUR, loginUTILISATEUR, nomUTILISATEUR, prenomUTILISATEUR, telephoneUTILISATEUR, mailUTILISATEUR, departementUTILISATEUR, mdpUTILISATEUR, admin) VALUES
 (1, 'ADMIN', 'ADMIN', 'ADMIN', 0000000000, 'admin@admin.com', 000, '$2y$10$W8FT9xv1/.sXchxzsJ3uleAltP6WUSB9Kl6hFH4ArRHkmvWZm8Bkq', 1),
 (2, 'Raphaël', 'Hammonais', 'Raphaël', 0102030405, 'raphael@toto.com', 048, '$2y$10$RjlMN7meHW3Soryr/bWO9eYF0wjdvOZ.5fjJPJtDjcfJpERH1FIJe', 0);
 
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `camping`
---
-ALTER TABLE `camping`
-  ADD PRIMARY KEY (`idcamping`);
-
---
--- Index pour la table `categorie`
---
-ALTER TABLE `categorie`
-  ADD PRIMARY KEY (`idcategorie`);
-
---
--- Index pour la table `logement`
---
-ALTER TABLE `logement`
-  ADD PRIMARY KEY (`idlogement`), ADD UNIQUE KEY `nomlogement` (`nomlogement`), ADD KEY `FK_logement_camping` (`idcamping`), ADD KEY `FK_logement_categorie` (`idcategorie`);
-
---
--- Index pour la table `pourcent_reduc`
---
-ALTER TABLE `pourcent_reduc`
-  ADD PRIMARY KEY (`avance`), ADD UNIQUE KEY `unique_avance` (`avance`);
-
---
--- Index pour la table `reservation`
---
-ALTER TABLE `reservation`
-  ADD PRIMARY KEY (`numreservation`), ADD KEY `FK_reservation_utilisateur` (`idUTILISATEUR`), ADD KEY `FK_reservation_logement` (`idlogement`);
-
---
--- Index pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  ADD PRIMARY KEY (`idUTILISATEUR`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `camping`
---
-ALTER TABLE `camping`
-  MODIFY `idcamping` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
---
--- AUTO_INCREMENT pour la table `categorie`
---
-ALTER TABLE `categorie`
-  MODIFY `idcategorie` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT pour la table `logement`
---
-ALTER TABLE `logement`
-  MODIFY `idlogement` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
---
--- AUTO_INCREMENT pour la table `reservation`
---
-ALTER TABLE `reservation`
-  MODIFY `numreservation` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT pour la table `utilisateur`
---
-ALTER TABLE `utilisateur`
-  MODIFY `idUTILISATEUR` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
 
 --
--- Contraintes pour la table `logement`
+-- Contraintes pour la table logement
 --
-ALTER TABLE `logement`
-ADD CONSTRAINT `FK_logement_camping` FOREIGN KEY (`idcamping`) REFERENCES `camping` (`idcamping`) ON DELETE CASCADE,
-ADD CONSTRAINT `FK_logement_categorie` FOREIGN KEY (`idcategorie`) REFERENCES `categorie` (`idcategorie`) ON DELETE CASCADE;
+ALTER TABLE logement
+  ADD CONSTRAINT FK_logement_camping FOREIGN KEY (idcamping) REFERENCES camping (idcamping),
+  ADD CONSTRAINT FK_logement_categorie FOREIGN KEY (idcategorie) REFERENCES categorie (idcategorie);
 
 --
--- Contraintes pour la table `reservation`
+-- Contraintes pour la table reservation
 --
-ALTER TABLE `reservation`
-ADD CONSTRAINT `FK_reservation_logement` FOREIGN KEY (`idlogement`) REFERENCES `logement` (`idlogement`) ON DELETE CASCADE,
-ADD CONSTRAINT `FK_reservation_utilisateur` FOREIGN KEY (`idUTILISATEUR`) REFERENCES `utilisateur` (`idUTILISATEUR`) ON DELETE CASCADE;
+ALTER TABLE reservation
+  ADD CONSTRAINT FK_reservation_logement FOREIGN KEY (idlogement) REFERENCES logement (idlogement),
+  ADD CONSTRAINT FK_reservation_utilisateur FOREIGN KEY (idUTILISATEUR) REFERENCES utilisateur (idUTILISATEUR);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+ALTER TABLE utilisateur ADD UNIQUE(mailUTILISATEUR);
